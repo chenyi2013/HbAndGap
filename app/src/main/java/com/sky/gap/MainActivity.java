236d090f6ce1120df4eb67f07dd5f34676bf6577
妙手金刚 <1136096189@qq.com>
2016-01-21 18:43:46 +0800

    sky

---
+++ app/src/main/java/com/sky/gap/MainActivity.java
@@ -0,0 +1,207 @@
+package com.sky.gap;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private ProgressBar mProgressBar = null;
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            String url = (String) msg.obj;
+            url = url.replace("\"", "");
+            webView.loadUrl(url.toString().trim());
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        webView = (WebView) findViewById(R.id.webView);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        setWebView();
+
+        RequestQueue mQueue = Volley.newRequestQueue(getApplicationContext());
+        mQueue.add(new StringRequest(Request.Method.GET, "http://www.coriver.cn/portal/FcustomAction/getVersion.action", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Message message = handler.obtainMessage();
+                message.obj = response;
+                handler.sendMessage(message);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }));
+        mQueue.start();
+
+    }
+
+    private void setWebView() {
+        // 设置支持JavaScript脚本
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        // 设置可以访问文件
+        webSettings.setAllowFileAccess(true);
+        // 设置可以支持缩放
+        webSettings.setSupportZoom(true);
+        // 设置默认缩放方式尺寸是far
+        webSettings.setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
+        // 设置出现缩放工具
+        webSettings.setBuiltInZoomControls(false);
+        webSettings.setDefaultFontSize(20);
+
+
+        // 设置WebViewClient
+        webView.setWebViewClient(new WebViewClient() {
+            // url拦截
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // 使用自己的WebView组件来响应Url加载事件，而不是使用默认浏览器器加载页面
+                view.loadUrl(url);
+                // 相应完成返回true
+                return true;
+                // return super.shouldOverrideUrlLoading(view, url);
+            }
+
+            // 页面开始加载
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                mProgressBar.setVisibility(View.VISIBLE);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            // 页面加载完成
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                mProgressBar.setVisibility(View.GONE);
+                super.onPageFinished(view, url);
+            }
+
+            // WebView加载的所有资源url
+            @Override
+            public void onLoadResource(WebView view, String url) {
+                super.onLoadResource(view, url);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode,
+                                        String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                Toast.makeText(MainActivity.this, "description===" + description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // 设置WebChromeClient
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            // 处理javascript中的alert
+            public boolean onJsAlert(WebView view, String url, String message,
+                                     final JsResult result) {
+                return super.onJsAlert(view, url, message, result);
+            }
+
+            ;
+
+            @Override
+            // 处理javascript中的confirm
+            public boolean onJsConfirm(WebView view, String url,
+                                       String message, final JsResult result) {
+                return super.onJsConfirm(view, url, message, result);
+            }
+
+            ;
+
+            @Override
+            // 处理javascript中的prompt
+            public boolean onJsPrompt(WebView view, String url, String message,
+                                      String defaultValue, final JsPromptResult result) {
+                return super.onJsPrompt(view, url, message, defaultValue,
+                        result);
+            }
+
+            ;
+
+            // 设置网页加载的进度条
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                mProgressBar.setProgress(newProgress);
+                super.onProgressChanged(view, newProgress);
+            }
+
+            // 设置程序的Title
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+            }
+        });
+    }
+
+    /**
+     * 描述：能退出吗
+     *
+     * @return
+     * @throws
+     * @date：2013-12-13 上午11:06:58
+     * @version v1.0
+     */
+    public boolean canBack() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+            return false;
+        }
+        return true;
+    }
+
+    private long lastBack;
+
+    @Override
+    public void onBackPressed() {
+        if (canBack()) {
+            long nowCurrent = System.currentTimeMillis();
+            if (nowCurrent - lastBack > 3000) {
+                Toast.makeText(this, "再按一次返回键回到桌面", Toast.LENGTH_SHORT).show();
+                lastBack = nowCurrent;
+            } else {
+                super.onBackPressed();
+            }
+        }
+    }
+}
